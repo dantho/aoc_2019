2be9f44a27dfa9dde34f775acb38a0976755062f
Dan Thornton <dan.thornton+git@thornton.me>
2019-12-10 19:52:44 -0500

    Refactored with async streams -- PASS for Part 1

--- dantho/day07/src/main.rs
+++ dantho/day07/src/main.rs
@@ -2,112 +2,149 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-fn intcode_run(v: &mut Vec<isize>, machine_input: Vec<isize>, machine_output: &mut Vec<isize>){
-    let mut i: usize = 0;
-    let mut input = machine_input.clone();
+use futures::prelude::*;
+use futures::channel::mpsc::{channel,Sender,Receiver};
+use futures::executor::block_on;
+use futures::join;
+
+async fn intcode_run(mut v: Vec<isize>, mut input: Receiver<isize>, mut output: Sender<isize>) -> Receiver<isize> {
+    let mut pc: usize = 0;
     loop {
-        let mode = v[i] / 100;
-        let op = v[i] - mode * 100;
+        let mode = v[pc] / 100;
+        let op = v[pc] - mode * 100;
         let m1 = mode - mode / 10 * 10;  let mode = mode / 10;
         let m2 = mode - mode / 10 * 10;  let mode = mode / 10;
         let m3 = mode - mode / 10 * 10;  let mode = mode / 10;
         assert_eq!(mode, 0);
         match op {
             1 => {
-                let p1 = v[i + 1];
-                let p2 = v[i + 2];
-                let p3 = v[i + 3];
+                let p1 = v[pc + 1];
+                let p2 = v[pc + 2];
+                let p3 = v[pc + 3];
                 let v1 = if m1 == 0 {v[p1 as usize]} else {p1};
                 let v2 = if m2 == 0 {v[p2 as usize]} else {p2};
                 assert_eq!(m3, 0);
                 v[p3 as usize] = v1 + v2;
-                i += 4;
+                pc += 4;
             }
             2 => {
-                let p1 = v[i + 1];
-                let p2 = v[i + 2];
-                let p3 = v[i + 3];
+                let p1 = v[pc + 1];
+                let p2 = v[pc + 2];
+                let p3 = v[pc + 3];
                 let v1 = if m1 == 0 {v[p1 as usize]} else {p1};
                 let v2 = if m2 == 0 {v[p2 as usize]} else {p2};
                 assert_eq!(m3, 0);
                 v[p3 as usize] = v1 * v2;
-                i += 4;
+                pc += 4;
             }
             3 => {
-                let p1 = v[i + 1];
+                let p1 = v[pc + 1];
                 assert_eq!(m1, 0);
-                v[p1 as usize] = match input.pop() {
+                v[p1 as usize] = match input.next().await {
                     Some(v) => v,
-                    None => panic!("Input buffer empty."),
+                    None => panic!("Expecting input, but stream has terminated."),
                 };
-                i += 2;
+                pc += 2;
             }
             4 => {
-                let p1 = v[i + 1];
+                let p1 = v[pc + 1];
                 let v1 = if m1 == 0 {v[p1 as usize]} else {p1};
-                machine_output.push(v1);
-                i += 2;
+                if let Err(_) = output.send(v1).await {
+                    panic!("Problem sending output data, has receiver been dropped?")
+                };
+                pc += 2;
             }
             5 => {
-                let p1 = v[i + 1];
-                let p2 = v[i + 2];
+                let p1 = v[pc + 1];
+                let p2 = v[pc + 2];
                 let v1 = if m1 == 0 {v[p1 as usize]} else {p1};
                 let v2 = if m2 == 0 {v[p2 as usize]} else {p2};
                 if v1 != 0 {
-                    i = v2 as usize
+                    pc = v2 as usize
                 } else {
-                    i += 3
+                    pc += 3
                 };
             }
             6 => {
-                let p1 = v[i + 1];
-                let p2 = v[i + 2];
+                let p1 = v[pc + 1];
+                let p2 = v[pc + 2];
                 let v1 = if m1 == 0 {v[p1 as usize]} else {p1};
                 let v2 = if m2 == 0 {v[p2 as usize]} else {p2};
                 if v1 == 0 {
-                    i = v2 as usize
+                    pc = v2 as usize
                 } else {
-                    i += 3
+                    pc += 3
                 };
             }
             7 => {
-                let p1 = v[i + 1];
-                let p2 = v[i + 2];
-                let p3 = v[i + 3];
+                let p1 = v[pc + 1];
+                let p2 = v[pc + 2];
+                let p3 = v[pc + 3];
                 let v1 = if m1 == 0 {v[p1 as usize]} else {p1};
                 let v2 = if m2 == 0 {v[p2 as usize]} else {p2};
                 assert_eq!(m3, 0);
                 v[p3 as usize] = if v1 < v2 {1} else {0};
-                i += 4;
+                pc += 4;
             }
             8 => {
-                let p1 = v[i + 1];
-                let p2 = v[i + 2];
-                let p3 = v[i + 3];
+                let p1 = v[pc + 1];
+                let p2 = v[pc + 2];
+                let p3 = v[pc + 3];
                 let v1 = if m1 == 0 {v[p1 as usize]} else {p1};
                 let v2 = if m2 == 0 {v[p2 as usize]} else {p2};
                 assert_eq!(m3, 0);
                 v[p3 as usize] = if v1 == v2 {1} else {0};
-                i += 4;
+                pc += 4;
             }
             99 => break,
             x => panic!("Unexpected opcode {}", x),
         }
     }
+    input // Drop the input Receiver, this allow downstream fetching of values we are not going to process ('cause we're done)
 }
-fn amplifier_x5(v: &mut Vec<isize>, phase_x5: Vec<isize>, machine_input: isize, machine_output: &mut isize){
-    let mut input = machine_input;
-    let mut output = Vec::new();
+async fn amplifier_x5(prog: Vec<isize>, phase_x5: Vec<isize>, initial_input: isize) -> isize {
+    let mut amplifier_inputs: Vec<Receiver<isize>> = Vec::new();
+    let mut amplifier_outputs: Vec<Sender<isize>> = Vec::new();
     assert_eq!(phase_x5.len(), 5);
+    // Create input/output connections ("wires")
     for phase in phase_x5.into_iter() {
-        let mut prog = v.clone();
-        let mut input_vec = Vec::new();
-        input_vec.push(input);
-        input_vec.push(phase);
-        intcode_run(&mut prog, input_vec, &mut output);
-        input = output.pop().unwrap();
+        const BUFFER_SIZE: usize = 10;
+        let (mut output_tx, input_rx) = channel::<isize>(BUFFER_SIZE);
+        output_tx.send(phase).await.unwrap();
+        // add initial_input to first amplifier ONLY
+        if 0 == amplifier_inputs.len()  {
+            output_tx.send(initial_input).await.unwrap();
+        };
+        amplifier_inputs.push(input_rx);
+        amplifier_outputs.push(output_tx);
     }
-    *machine_output = input;
+    // Shift output order such that inputs come from prior amplifier, (or LAST amplifier for first amplifier input)
+    // In other words, move first output to last position and shift all the others down one.  Done.
+    amplifier_outputs.reverse();
+    let tmp = amplifier_outputs.pop().unwrap();
+    amplifier_outputs.reverse();
+    amplifier_outputs.push(tmp);
+    // Create/connect all amplifiers to produce a Vec of futures
+    let amplifier_future_results: Vec<_> =
+        amplifier_inputs.into_iter()
+        .zip(amplifier_outputs.into_iter())
+        .map(|(input_stream,output_stream)| {    
+            intcode_run(prog.clone(), input_stream, output_stream)
+        }).collect();
+    assert_eq!(amplifier_future_results.len(), 5);
+    let mut a = amplifier_future_results;
+    let mut result_streams = join!(
+        a.pop().expect("Amplifier E result stream not found."),
+        a.pop().expect("Amplifier D result stream not found."),
+        a.pop().expect("Amplifier C result stream not found."),
+        a.pop().expect("Amplifier B result stream not found."),
+        a.pop().expect("Amplifier A result stream not found."),
+    );
+    // Based on dataflow, and assuming each amplifier terminates cleanly in order,
+    // the last channel should have one final result "in the pipe".
+    // BUT, the last amplifier's output receiver is connect to the first amplifier's input
+    // SO, we need the first amplifier's returned receiver.
+    (result_streams.4).next().await.expect("Amplifier Output channel EMPTY.")
 }
 fn main() {
     let filename = "day07_input.txt";
@@ -118,16 +155,14 @@ fn main() {
     // let filename = "day07_example5.txt";
     let fd = File::open(filename).expect(&format!("Failure opening {}", filename));
     let buf = BufReader::new(fd);
-    let mut v_orig = Vec::new();
+    let mut prog_orig = Vec::new();
     buf.lines().for_each(|line| {
         line.unwrap().split(',').for_each(|numstr| {
             let num = numstr.parse::<isize>().unwrap();
-            v_orig.push(num);
+            prog_orig.push(num);
         });
     });
-    let mut v = v_orig.clone();
-    let input = 0;
-    let mut out: isize = 999999;
+    let initial_input = 0;
     let mut max_out = std::isize::MIN;
     let phases0 = vec![4,3,2,1,0];
     for ph0 in 0..5 {
@@ -144,9 +179,10 @@ fn main() {
                     phases4.remove(ph3);
                     for ph4 in 0..1 {
                         let phase = vec![phases4[ph4],phases3[ph3],phases2[ph2],phases1[ph1],phases0[ph0]];
-                        amplifier_x5(&mut v, phase.clone(), input, &mut out);
+                        let phase_display = format!("{:?}", phase);
+                        let out = block_on(amplifier_x5(prog_orig.clone(), phase, initial_input));
                         if out > max_out {
-                            println!("Part 1) Max thruster signal {} (from phase setting sequence {:?})", out, phase);
+                            println!("Part 1) Max thruster signal {} (from phase setting sequence {:?})", out, phase_display);
                             max_out = out;
                         }
                     }
