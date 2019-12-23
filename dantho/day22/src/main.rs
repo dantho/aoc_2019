49d5a92c809d361a42a3cbbd83c868df73eab254
Dan Thornton <dan.thornton+git@thornton.me>
2019-12-23 11:31:17 -0500

    Bailing on lazy iterator chain - using Vec instead

--- dantho/day22/src/main.rs
+++ dantho/day22/src/main.rs
@@ -1,95 +1,103 @@
 /// https://adventofcode.com/2019/day/22
 fn main() -> Result<(),Error> {
-    let deck: Deck = Deck::new(0..10);
+    let deck = 0..10;
+    let deck = increment_n(deck, 7, 10);
+    let deck = new_stack(deck);
+    let deck = new_stack(deck);
+    // let deck = cut_n(deck, 4, 10);
+    // let deck = increment_n(deck, 3, 10);
+    println!("deck: {:?}", deck);
     Ok(())
 }
 #[derive(Debug)]
 enum Error {
-    IllegalMapData {ch: char},
-    MapAssertFail {msg: String},
+    NotImplemented,
+    // MapAssertFail {msg: String},
 }
-#[derive(Debug, PartialEq)]
-struct Deck (Vec<usize>);
-impl Deck {
-    fn new(range: std::ops::Range<usize>) -> Self {
-        Deck (range.collect())
-    }
-    fn new_stack(cards: Iterator<Item = usize> + DoubleEndedIterator) -> Self {
-        cards.rev()
-    }
-    fn cut_n_cards(self, n: usize) -> Self {
-        let size = self.0.len();
-        Deck (self.into_iter().cycle().skip(n).take(size).collect::<Vec<_>>())
-    }
-    fn increment_n(self, n: isize) -> Self {
-        let n_pos = if n >= 0 {n as usize} else {(self.0.len() as isize + n) as usize};
-        Deck (self.0.iter().cycle().step_by(n_pos).map(|v|{*v}).take(self.0.len()).collect::<Vec<_>>())
-    }
-    fn shuffle(self, instructions: &str) -> Result<Self,Error> {
-        Ok(self)
-    }
+fn new_stack<T>(cards: T) -> std::iter::Rev<T>
+where T: Iterator<Item=usize>+DoubleEndedIterator<Item=usize> {
+    cards.rev()
 }
-impl IntoIterator for Deck {
-    type Item = usize;
-    type IntoIter = DeckIntoIterator;
+fn cut_n<T>(cards: T, n: isize, size: usize) -> std::vec::IntoIter<usize>
+where T: Clone+Iterator<Item=usize>+DoubleEndedIterator<Item=usize> {
+    let n = if n < 0 {size as isize + n} else {n} as usize;
+    cards.cycle().skip(n).take(size).collect::<Vec<usize>>().into_iter()
+}
+fn increment_n<T>(cards: T, n: usize, size: usize) -> std::vec::IntoIter<usize>
+where T: Clone+Iterator<Item=usize>+DoubleEndedIterator<Item=usize> {
+    cards.cycle().step_by(size-n).take(size).collect::<Vec<usize>>().into_iter()
+}
+fn shuffle<T>(cards: T, input: &str) -> Vec<usize>
+where T: Clone+Iterator<Item=usize>+DoubleEndedIterator<Item=usize> {
+    let deck_size = cards.clone().count();
+    let mut new_deal = cards;
+    for line in input.lines() {
+        let line = line.trim();
+        if line.len() == 0 {continue};
+        new_deal =
+            if line.contains("deal into new stack") {
+                new_stack(new_deal)
+            } else if line.contains("deal with increment "){
+                let pieces = line.split("deal with increment ");
+                if pieces.len() == 2 {
+                    if pieces[0].len() == 0 {
+                        let param = pieces[1].parse().unwrap();
+                        increment_n(new_deal, param, deck_size)
+                    }
+                }
+            } else if line.contains("cut ") {
+                let pieces = line.split("cut ");
+                if pieces.len() == 2 {
+                    if pieces[0].len() == 0 {
+                        let param = pieces[1].parse().unwrap();
+                        cut_n(new_deal, param, deck_size)
+                    }
+                }
+            } else {
+                panic!(format!("Unknown input line: {}", line));
+            };
+    }
+    new_deal.collect::<Vec<usize>>()
+}
 
-    fn into_iter(self) -> Self::IntoIter {
-        DeckIntoIterator {
-            deck: self,
-            index: 0,
-        }
-    }
-}
-
-struct DeckIntoIterator {
-    deck: Deck,
-    index: usize,
-}
-impl Iterator for DeckIntoIterator {
-    type Item = usize;
-    fn next(&mut self) -> Option<usize> {
-        let result = match self.index {
-            self.pop().unwrap
-        };
-        self.index += 1 mod;
-        Some(result)
-    }
-}
 #[test]
 fn test_increment_n() {
-    let deck = Deck::new(0..10);
-    let deck = deck.increment_n(3);
-    assert_eq!(deck.0, vec![0,3,6,9,2,5,8,1,4,7]);
+    let deck = 0..10;
+    let deck = increment_n(deck, 3, 10).collect::<Vec<usize>>();
+    assert_eq!(deck, vec![0,7,4,1,8,5,2,9,6,3]);
 }
 #[test]
 fn test_cut_n_cards() {
-    let deck = Deck::new(0..10);
-    let deck = deck.cut_n_cards(3);
-    assert_eq!(deck.0, vec![3,4,5,6,7,8,9,0,1,2]);
+    let deck = 0..10;
+    let deck = cut_n(deck, 3, 10).collect::<Vec<usize>>();
+    assert_eq!(deck, vec![3,4,5,6,7,8,9,0,1,2]);
+    let deck = 0..10;
+    let deck = cut_n(deck, -2, 10).collect::<Vec<usize>>();
+    assert_eq!(deck, vec![8,9,0,1,2,3,4,5,6,7]);
 }
 #[test]
 fn test_new_stack() {
-    let deck = Deck::new(0..10);
-    let deck = deck.new_stack();
-    assert_eq!(deck.0, vec![9,8,7,6,5,4,3,2,1,0]);
+    let deck = 0..10;
+    let deck = new_stack(deck).collect::<Vec<usize>>();
+    assert_eq!(deck, vec![9,8,7,6,5,4,3,2,1,0]);
 }
-#[test]
-fn ex1() {example(EX1, vec![0, 3, 6, 9, 2, 5, 8, 1, 4, 7]);}
-#[test]
-fn ex2() {example(EX1, vec![3, 0, 7, 4, 1, 8, 5, 2, 9, 6]);}
-#[test]
-fn ex3() {example(EX1, vec![6, 3, 0, 7, 4, 1, 8, 5, 2, 9]);}
-#[test]
-fn ex4() {example(EX1, vec![9, 2, 5, 8, 1, 4, 7, 0, 3, 6]);}
+// #[test]
+// fn ex1() {example(EX1, vec![0, 3, 6, 9, 2, 5, 8, 1, 4, 7]);}
+// #[test]
+// fn ex2() {example(EX1, vec![3, 0, 7, 4, 1, 8, 5, 2, 9, 6]);}
+// #[test]
+// fn ex3() {example(EX1, vec![6, 3, 0, 7, 4, 1, 8, 5, 2, 9]);}
+// #[test]
+// fn ex4() {example(EX1, vec![9, 2, 5, 8, 1, 4, 7, 0, 3, 6]);}
 
-fn example(input: &'static str, result: Vec<usize>) -> Result<(),Error> {
-    let deck = Deck::new(0..10);
-    let instructions = input;
-    let shuffle_result:Vec<usize> = deck.shuffle(instructions)?.into_iter().collect();
-    let expected_result = result;
-    assert_eq!(shuffle_result, expected_result);
-    Ok(())
-}
+// fn example(input: &'static str, result: Vec<usize>) -> Result<(),Error> {
+//     let deck = Deck::new(0..10);
+//     let instructions = input;
+//     let shuffle_result:Vec<usize> = deck.shuffle(instructions)?.into_iter().collect();
+//     let expected_result = result;
+//     assert_eq!(shuffle_result, expected_result);
+//     Ok(())
+// }
 const EX1: &'static str = r#"
 deal with increment 7
 deal into new stack
