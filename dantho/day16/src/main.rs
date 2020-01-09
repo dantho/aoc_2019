5360dea363cd33be018aed2d8bc3839f3690c330
Dan Thornton <dan.thornton+git@thornton.me>
2020-01-09 18:02:14 -0500

    Day 16 Part 2 still trying still failing

--- dantho/day16/src/main.rs
+++ dantho/day16/src/main.rs
@@ -1,40 +1,48 @@
 /// https://adventofcode.com/2019/day/16
 use std::char::from_digit;
+const BASE_REPEAT: usize = 10_000-3;
 
 fn main() {
 
-    println!("5978783 Mod 650 is {}", 5978783 % 650);
-    println!("0303673 Mod 32 is {}", 0303673 % 32);
-    println!("0293510 Mod 32 is {}", 0293510 % 32); // 6 (is less than 8) -- This will be a problem until we implement the edge case
-    println!("0308177 Mod 32 is {}", 0308177 % 32);
+    println!("    5978783 Mod 650 is {}", 5978783 % 650);
+    println!("EX5 0303673 Mod  32 is {}", 0303673 % 32);
+    println!("EX6 0293510 Mod  32 is {}", 0293510 % 32); // 6 (is less than 8) -- This will be a problem until we implement the edge case
+    println!("EX7 0308177 Mod  32 is {}", 0308177 % 32);
 
     // println!("EX5: Sum of 03036732577212944063491565474664 is {}", 0+3+0+3+6+7+3+2+5+7+7+2+1+2+9+4+4+0+6+3+4+9+1+5+6+5+4+7+4+6+6+4);
-    println!("EX7: Sum of 03081770884921959731165446850517 is {}", 0+3+0+8+1+7+7+0+8+8+4+9+2+1+9+5+9+7+3+1+1+6+5+4+4+6+8+5+0+5+1+7);
+    println!("EX7: Sum of 03081770884921959731165446850517 is {}", 0+3+0+8+1+7+7+0+8+8+4+9+2+1+9+5+9+7+3+1+1+6+5+4+4+6+8+5+0+5+1+7); // EX7 -- 144
     println!("100 Phase Calc (Part 2): {}", process(EX7, 100));
     let _avoid_unused_warning = [INPUT, EX5, EX6, EX7];
 }
-// WARNING:  EDGE CASE NOT YET IMPLEMENTED! -- WHERE starting_ndx lands WITHIN 8 DIGITS OF END OF INPUT.  :(
+// WARNING:  EDGE CASE NOT YET IMPLEMENTED! -- WHERE starting_ndx lands WITHIN 8 DIGITS OF END OF INPUT.  :(  See EX5.
 fn process(input: &'static str, phase_cnt: u32) -> String {
     let input_len = input.len();
-    let offset: usize = digits(input, 7, 0); // "first seven digits" is message offset
+    let offset: usize = str2digits(input, 7, 0); // "first seven digits" is message offset
     let starting_ndx = offset % input_len;
     let mut in_digits: Vec<usize> = input.chars().map(|c|c.to_digit(10).unwrap() as usize).collect();
     for _phase in 0..phase_cnt {
-        let sum_base_input_digits: usize = in_digits.iter().sum();
-        // println!("Base input digits sum to {}", sum_base_input_digits);
-        let sum_remaining_input_blocks = sum_base_input_digits * ((input_len*10_000 - offset)/input_len);
-        // println!("Sum of remaining ({}) full blocks is {}", (input_len*10_000 - offset)/input_len, sum_remaining_input_blocks);
+        let sum_base_input_digits: usize = in_digits.iter().sum::<usize>();
+        let debug_sum_remaining_input_blocks = sum_base_input_digits * ((input_len*BASE_REPEAT - offset)/input_len);
+        let sum_remaining_input_blocks = sum_base_input_digits * ((input_len*BASE_REPEAT - offset)/input_len) % 10;
         let mut rhs_digits: Vec<usize> = Vec::new();
         for dig_ndx in 0..input_len {
-            rhs_digits.push((in_digits.iter().skip(dig_ndx).sum::<usize>() + sum_remaining_input_blocks) % 10);
+            let x = (in_digits.iter().skip(dig_ndx).sum::<usize>() + sum_remaining_input_blocks) % 10;
+            rhs_digits.push(x);
         }
+        println!("Phase {}: {} {} from {} in remaining", _phase, digits2string(&in_digits), sum_remaining_input_blocks, debug_sum_remaining_input_blocks);
         in_digits = rhs_digits;
-        // println!("Phase {}:  {:?}", _phase, in_digits);
     }
-    // return 1st 8 digits at starting at "message offset"
+    println!("Phase {}: {}", phase_cnt, digits2string(&in_digits));
     in_digits.iter().skip(starting_ndx).take(8).fold(0, |prev, d|{d + 10*prev}).to_string()
 }
-fn digits(input: &'static str, size: usize, index: usize) -> usize {
+fn digits2string(input: &Vec<usize>) -> String {
+    let mut ret_string = String::new();
+    for d in input {
+        ret_string.push_str(&d.to_string());
+    }
+    ret_string
+}
+fn str2digits(input: &'static str, size: usize, index: usize) -> usize {
     input.split_at(index).1.split_at(size).0.parse().unwrap()
 }
 fn digit(input: &'static str, index: usize) -> usize {
@@ -48,7 +56,7 @@ const EX7: &'static str = "03081770884921959731165446850517";
 
 #[test]
 fn test_part2_input() {
-    assert_eq!(process(INPUT, 100), "TBD".to_string());
+    assert_eq!(process(INPUT, 100), "xxxxxxxx".to_string()); // 26226606 is known to be wrong.  :(
 }
 #[test]
 fn test_part2_ex5() {
@@ -65,8 +73,8 @@ fn test_part2_ex7() {
 
 #[test]
 fn test_digits() {
-    assert_eq!(digits("123456789", 3, 1), 234);
-    assert_eq!(digits("123456789", 7, 0), 1234567);
+    assert_eq!(str2digits("123456789", 3, 1), 234);
+    assert_eq!(str2digits("123456789", 7, 0), 1234567);
 }
 #[test]
 fn test_digit() {
