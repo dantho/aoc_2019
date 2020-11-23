9a7ac4b147bc578de91f205cbe47d7de0feaff01
Dan Thornton <dan.thornton+git@thornton.me>
2020-11-23 09:26:26 -0500

    OMG!  Part 2 DONE!!  2nd Star earned after 1 year!

--- dantho/day16/src/main.rs
+++ dantho/day16/src/main.rs
@@ -1,10 +1,11 @@
 /// https://adventofcode.com/2019/day/16
 use std::iter::repeat;
+pub use num::traits::int::PrimInt;
 
 const _BASE_REPEAT: usize = 10_000;
 
 fn main() {
-    let _avoid_unused_warning = [SIGNAL, EX1, EX2, EX3, EX4, EX5, EX6];
+    let _avoid_unused_warning = [SIGNAL, EX0, EX1, EX2, EX3, EX4, EX5, EX6];
     let _avoid_unused_answers = [
         PART1_ANSWER,
         EX0_ANSWER1,
@@ -14,11 +15,16 @@ fn main() {
         EX1_ANSWER,
         EX2_ANSWER,
         EX3_ANSWER,
+    ];
+    let _avoid_unused_answers2 = [
         EX4_ANSWER,
         EX5_ANSWER,
         EX6_ANSWER,
     ];
-    println!("{} {}", part1(EX0, 1), EX0_ANSWER1);
+    println!("Signal length is {}", SIGNAL.len());
+    println!("EX4 is {}", part2(EX4, 100));
+    
+
 }
 trait ToDigits {
     fn to_digits(&self) -> Vec<u8>;
@@ -28,11 +34,31 @@ impl ToDigits for str {
         self.chars().map(|c|c.to_digit(10).unwrap() as u8).collect()
     }
 }
+
 pub fn part1(signal: &str, phase_cnt: u8) -> u64 {
     let input_digits = signal.to_digits();
     (1..=phase_cnt).fold(input_digits,|phase_input_digits, _|{
         do_phase(&phase_input_digits)
     }).iter().take(8).fold(0,|num,d|{num*10 + *d as u64})
+}
+
+pub fn part2(signal: &str, phase_cnt: u8) -> usize {
+    let input_digits = signal.to_digits();
+    let offset = input_digits.iter().take(7).fold(0,|num,d|{num*10 + *d as usize});
+    let length = input_digits.len();
+    let reverse_digits = input_digits.into_iter().rev().cycle().take(length*10_000-offset).collect();
+
+    (1..=phase_cnt).fold(reverse_digits,|phase_input_digits, _|{
+        do_phase_part2(phase_input_digits)
+    }).into_iter().rev().take(8).fold(0,|num,d|{num*10 + d as usize})
+}
+
+pub fn do_phase_part2(input: Vec<u8>) -> Vec<u8> {
+    input.into_iter().fold((0,Vec::new()),|(prev,mut rolling_sum), d| {
+        let mod_sum_so_far = (d + prev) % 10;
+        rolling_sum.push(mod_sum_so_far);
+        (mod_sum_so_far,rolling_sum)
+    }).1
 }
 
 pub fn do_phase(input: &Vec<u8>) -> Vec<u8> {
@@ -81,6 +107,7 @@ fn test_last_digit() {
 const SIGNAL: &'static str =
 "59787832768373756387231168493208357132958685401595722881580547807942982606755215622050260150447434057354351694831693219006743316964757503791265077635087624100920933728566402553345683177887856750286696687049868280429551096246424753455988979991314240464573024671106349865911282028233691096263590173174821612903373057506657412723502892841355947605851392899875273008845072145252173808893257256280602945947694349746967468068181317115464342687490991674021875199960420015509224944411706393854801616653278719131946181597488270591684407220339023716074951397669948364079227701367746309535060821396127254992669346065361442252620041911746738651422249005412940728";
 const PART1_ANSWER: u64 = 42945143;
+const PART2_ANSWER: usize = 99974970;
 const EX0: &'static str = "12345678";
 const EX0_ANSWER1: u64 = 48226158;
 const EX0_ANSWER2: u64 = 34040438;
@@ -93,15 +120,19 @@ const EX2_ANSWER: u64 = 73745418;
 const EX3: &'static str = "69317163492948606335995924319873";
 const EX3_ANSWER: u64 = 52432133;
 const EX4: &'static str = "03036732577212944063491565474664";
-const EX4_ANSWER: u64 = 84462026;
+const EX4_ANSWER: usize = 84462026;
 const EX5: &'static str = "02935109699940807407585447034323";
-const EX5_ANSWER: u64 = 78725270;
+const EX5_ANSWER: usize = 78725270;
 const EX6: &'static str = "03081770884921959731165446850517";
-const EX6_ANSWER: u64 = 53553731;
+const EX6_ANSWER: usize = 53553731;
 
 #[test]
 fn test_part1() {
     assert_eq!(part1(SIGNAL, 100), PART1_ANSWER);
+}
+#[test]
+fn test_part2() {
+    assert_eq!(part2(SIGNAL, 100), PART2_ANSWER);
 }
 #[test]
 fn test_ex0() {
@@ -123,15 +154,15 @@ fn test_ex3() {
     assert_eq!(part1(EX3, 100), EX3_ANSWER);
 }
 // Part2
-// #[test]
-// fn test_ex4() {
-//     assert_eq!(part2(EX4, 100), EX4_ANSWER);
-// }
-// #[test]
-// fn test_ex5() {
-//     assert_eq!(part2(EX5, 100), EX5_ANSWER);
-// }
-// #[test]
-// fn test_ex6() {
-//     assert_eq!(part2(EX6, 100), EX6_ANSWER);
-// }
+#[test]
+fn test_ex4() {
+    assert_eq!(part2(EX4, 100), EX4_ANSWER);
+}
+#[test]
+fn test_ex5() {
+    assert_eq!(part2(EX5, 100), EX5_ANSWER);
+}
+#[test]
+fn test_ex6() {
+    assert_eq!(part2(EX6, 100), EX6_ANSWER);
+}
